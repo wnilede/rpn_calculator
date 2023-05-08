7e7256fa1e739bd97427bf4241a7364a3114f9d1
wilhelm nilede <wnilede@gmail.com>
2023-05-08 15:26:55 +0200

    Remove tests that inverted should not pass.

--- test/layout_optimization_test.dart
+++ test/layout_optimization_test.dart
@@ -217,22 +217,6 @@ void main() {
           expectedWidth: 100,
           expectedHeight: 100)
     ]);
-    testLayoutOptimizer('2 too big children in column', 100, 300, inv.strategy, [
-      TestableWidget(
-          parameters: inv.Parameters(
-            preferredWidth: 100,
-            preferredHeight: 200,
-          ),
-          expectedWidth: 100,
-          expectedHeight: 150),
-      TestableWidget(
-          parameters: inv.Parameters(
-            preferredWidth: 100,
-            preferredHeight: 200,
-          ),
-          expectedWidth: 100,
-          expectedHeight: 150)
-    ]);
     testLayoutOptimizer('2 too large children in column', 100, 100, inv.strategy, [
       TestableWidget(
           parameters: inv.Parameters(
@@ -271,52 +255,6 @@ void main() {
           ),
           expectedWidth: 100,
           expectedHeight: 100)
-    ]);
-    testLayoutOptimizer('3 children in column in row perfectly', 100, 100, inv.strategy, [
-      TestableWidget(
-          parameters: inv.Parameters(
-            preferredWidth: 50,
-            preferredHeight: 100,
-          ),
-          expectedWidth: 50,
-          expectedHeight: 100),
-      TestableWidget(
-          parameters: inv.Parameters(
-            preferredWidth: 50,
-            preferredHeight: 50,
-          ),
-          expectedWidth: 50,
-          expectedHeight: 50),
-      TestableWidget(
-          parameters: inv.Parameters(
-            preferredWidth: 50,
-            preferredHeight: 50,
-          ),
-          expectedWidth: 50,
-          expectedHeight: 50)
-    ]);
-    testLayoutOptimizer('3 too small children in row in column', 10, 10, inv.strategy, [
-      TestableWidget(
-          parameters: inv.Parameters(
-            preferredWidth: 100,
-            preferredHeight: 50,
-          ),
-          expectedWidth: 10,
-          expectedHeight: 5),
-      TestableWidget(
-          parameters: inv.Parameters(
-            preferredWidth: 50,
-            preferredHeight: 50,
-          ),
-          expectedWidth: 5,
-          expectedHeight: 5),
-      TestableWidget(
-          parameters: inv.Parameters(
-            preferredWidth: 50,
-            preferredHeight: 50,
-          ),
-          expectedWidth: 5,
-          expectedHeight: 5)
     ]);
   });
 }
