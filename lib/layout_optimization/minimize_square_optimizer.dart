8950338843fd02c475ec1fc1826459cf15ce6394
wilhelm nilede <wnilede@gmail.com>
2023-05-08 13:46:40 +0200

    Begin creating other parameter class

--- lib/layout_optimization/minimize_square_optimizer.dart
+++ lib/layout_optimization/minimize_square_optimizer.dart
@@ -12,24 +12,27 @@ double calculateBadnessForSize(Parameters parameters, Size size) {
   if (size.width < 0 || size.height < 0) {
     return double.infinity;
   }
-  return parameters.badnessCoefficients.width * (size.width - parameters.preferredSize.width).squared + parameters.badnessCoefficients.height * (size.height - parameters.preferredSize.height).squared + parameters.constantBadness;
+  final widthParameter = parameters._getDirectedParameterWidth(size.width);
+  final heightParameter = parameters._getDirectedParameterWidth(size.height);
+  return widthParameter.badnessCoefficients * (size.width - widthParameter.preferredSize).squared + heightParameter.badnessCoefficients * (size.height - heightParameter.preferredSize).squared + widthParameter.constantBadness + heightParameter.constantBadness;
 }
 
 List<double> getOptimalWidths(List<Parameters> parametersChildren, Size confinement) {
   final result = List<double>.filled(parametersChildren.length, 1);
+  final widthParameters = parametersChildren.map((child) => child._getDirectedParameterWidth(confinement.width)).toList();
   while (true) {
     double sumPreferred = 0;
     double sumInvertedCoefficients = 0;
     for (int i = 0; i < parametersChildren.length; i++) {
       if (result[i] > 0) {
-        sumPreferred += parametersChildren[i].preferredSize.width;
-        sumInvertedCoefficients += 1 / parametersChildren[i].badnessCoefficients.width;
+        sumPreferred += widthParameters[i].preferredSize;
+        sumInvertedCoefficients += 1 / widthParameters[i].badnessCoefficients;
       }
     }
     bool newVanishing = false;
     for (int i = 0; i < parametersChildren.length; i++) {
       if (result[i] > 0) {
-        result[i] = parametersChildren[i].preferredSize.width + (confinement.width - sumPreferred) / sumInvertedCoefficients / parametersChildren[i].badnessCoefficients.width;
+        result[i] = widthParameters[i].preferredSize + (confinement.width - sumPreferred) / sumInvertedCoefficients / widthParameters[i].badnessCoefficients;
         if (result[i] < 0) {
           result[i] = 0;
           newVanishing = true;
@@ -47,45 +50,91 @@ Parameters getParametersOfRow(List<Parameters> parametersChildren) {
   if (parametersChildren.length == 1) {
     return parametersChildren[0];
   }
-  final double sumCoefficientsHeight = parametersChildren.sum((child) => child.badnessCoefficients.height);
-  final double sumCoefficientsTimesPreferredHeight = parametersChildren.sum((child) => child.badnessCoefficients.height * child.preferredSize.height);
-  return Parameters._fromRaw(
-    preferredSize: Size(
-      parametersChildren.sum((child) => child.preferredSize.width),
-      sumCoefficientsTimesPreferredHeight / sumCoefficientsHeight,
-    ),
-    badnessCoefficients: Size(
-      1 / parametersChildren.sum((child) => 1 / child.badnessCoefficients.width),
-      sumCoefficientsHeight,
-    ),
-    constantBadness: parametersChildren.sum((child) => child.constantBadness + child.badnessCoefficients.height * child.preferredSize.height.squared) - sumCoefficientsTimesPreferredHeight.squared / sumCoefficientsHeight,
+  _getWidthParametersOfRow(parametersChildren.map((child) => child.))
+}
+
+DirectedParameters _getWidthParametersOfRow(List<DirectedParameters> parametersChildren) {
+  assert(parametersChildren.isNotEmpty);
+  if (parametersChildren.length == 1) {
+    return parametersChildren[0];
+  }
+  return DirectedParameters(
+    preferredSize: parametersChildren.sum((child) => child.preferredSize),
+    badnessCoefficients: 1 / parametersChildren.sum((child) => 1 / child.badnessCoefficients),
+    constantBadness: parametersChildren.sum((child) => child.constantBadness),
   );
 }
 
-class Parameters extends OptimizationParameters<Parameters> {
-  final Size preferredSize;
-  final Size badnessCoefficients;
+DirectedParameters _getHeightParametersOfRow(List<DirectedParameters> parametersChildren) {
+  assert(parametersChildren.isNotEmpty);
+  if (parametersChildren.length == 1) {
+    return parametersChildren[0];
+  }
+  final double sumCoefficientsHeight = parametersChildren.sum((child) => child.badnessCoefficients);
+  final double sumCoefficientsTimesPreferredHeight = parametersChildren.sum((child) => child.badnessCoefficients * child.preferredSize);
+  return DirectedParameters(
+    preferredSize: sumCoefficientsTimesPreferredHeight / sumCoefficientsHeight,
+    badnessCoefficients: sumCoefficientsHeight,
+    constantBadness: parametersChildren.sum((child) => child.constantBadness + child.badnessCoefficients * child.preferredSize.squared) - sumCoefficientsTimesPreferredHeight.squared / sumCoefficientsHeight,
+  );
+}
+
+class DirectedParameters {
+  final double preferredSize;
+  final double badnessCoefficients;
   final double constantBadness;
+
+  DirectedParameters({
+    required this.preferredSize,
+    required this.badnessCoefficients,
+    required this.constantBadness,
+  });
+}
+
+class Parameters extends OptimizationParameters<Parameters> {
+  final List<DirectedParameters> horizontalParts;
+  final List<double> horizontalChangingPoints;
+  final List<DirectedParameters> verticalParts;
+  final List<double> verticalChangingPoints;
 
   Parameters({
     required double preferredWidth,
     required double preferredHeight,
     double? badnessMultiplierWidth,
     double? badnessMultiplierHeight,
-    this.constantBadness = 0,
-  })  : badnessCoefficients = Size((badnessMultiplierWidth ?? 1) / preferredWidth, (badnessMultiplierHeight ?? 1) / preferredHeight),
-        preferredSize = Size(preferredWidth, preferredHeight);
+    double constantBadness = 0,
+  })  : horizontalParts = [
+          DirectedParameters(
+            badnessCoefficients: (badnessMultiplierWidth ?? 1) / preferredWidth,
+            preferredSize: preferredWidth,
+            constantBadness: constantBadness,
+          ),
+        ],
+        verticalParts = [
+          DirectedParameters(
+            badnessCoefficients: (badnessMultiplierHeight ?? 1) / preferredHeight,
+            preferredSize: preferredHeight,
+            constantBadness: 0,
+          ),
+        ],
+        verticalChangingPoints = [],
+        horizontalChangingPoints = [];
 
   Parameters._fromRaw({
-    required this.preferredSize,
-    required this.badnessCoefficients,
-    required this.constantBadness,
-  });
+    required this.horizontalParts,
+    required this.horizontalChangingPoints,
+    required this.verticalParts,
+    required this.verticalChangingPoints,
+  }) : assert(horizontalParts.length == horizontalChangingPoints.length + 1 && verticalParts.length == verticalChangingPoints.length + 1);
 
   @override
   Parameters get transposed => Parameters._fromRaw(
-        preferredSize: preferredSize.flipped,
-        badnessCoefficients: badnessCoefficients.flipped,
-        constantBadness: constantBadness,
+        horizontalParts: verticalParts,
+        verticalParts: horizontalParts,
+        verticalChangingPoints: horizontalChangingPoints,
+        horizontalChangingPoints: horizontalChangingPoints,
       );
+
+  DirectedParameters _getDirectedParameterWidth(double width) => horizontalParts[width > horizontalChangingPoints.last ? horizontalChangingPoints.length : horizontalChangingPoints.indexWhere((changingPoint) => width > changingPoint)];
+  DirectedParameters _getDirectedParameterHeight(double height) => transposed._getDirectedParameterWidth(height);
 }
